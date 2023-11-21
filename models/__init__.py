609f91e2e73d5833193745c8f4e906b96df867dc
Eric Ubaga <ubaga91@gmail.com>
2023-11-21 10:41:07 +0300

    corrected the mysql setup, recruiters and db_storage files

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from models.applications import Application
+from models.base_model import Base, BaseModel
+from models.engine.db_storage import DBStorage
+from models.job_history import JobHistory
+from models.jobs import Jobs
+from models.jobseeker import Jobseeker
+from models.recruiters import Recruiter
+
+storage = DBStorage()
+
+storage.reload()
