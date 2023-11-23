cd28d98099cdfd36674ee5a80979da66c5f0c637
Sumshi <sumayasomow@gmail.com>
2023-11-23 06:25:50 +0000

    routes updated for all

--- web_static/app.py
+++ web_static/app.py
@@ -11,9 +11,26 @@ def login():
                            formMethod='post',
                            submitButtonText='Log In',
                            alternateButtonText='Sign Up')
+# @app.route('/signup')
+# def signUp():
+#     return render_template('signup.html')
+
 # displays home page
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/discover')
+def discover():
+    return render_template('discover.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
